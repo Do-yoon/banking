bb64e353e66c3e1734f515df8f292a9568ce47a5
Do-yoon <cheeezing_u@naver.com>
2021-06-21 16:35:21 +0900

    initial commit

---
+++ openbanking/src/main/java/com/doyoon/openbanking/v0/OpenbankingApplication.java
@@ -0,0 +1,13 @@
+package com.doyoon.openbanking.v0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenbankingApplication.class, args);
+    }
+
+}
---
+++ openbanking/src/main/java/com/doyoon/openbanking/v0/oauth/OAuthController.java
@@ -0,0 +1,4 @@
+package com.doyoon.openbanking.v0.oauth;
+
+public class OAuthController {
+}
