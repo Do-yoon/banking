75f5fd756d0813e9a71909f3a6fa274f3d1c7381
Do-yoon <cheeezing_u@naver.com>
2021-07-30 10:41:40 +0900

    added component

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = (
+    <div className="nav-bar" style="width:25%">
+        <a href="/" className="w3-bar-item w3-button">Link 1</a>
+        <a href="/" className="w3-bar-item w3-button">Link 2</a>
+        <a href="/" className="w3-bar-item w3-button">Link 3</a>
+    </div>
+);
+
+export default NavBar;
